488bb3b1a0136cc96869e442af7cb1de77d923db
Chris Kitzhaber <ckitzhaber@crimson.ua.edu>
2023-12-14 12:31:23 -0600

    phone test

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,14 @@
+let duration = 5000;
 let stopExecution = false;
+
 
 function sendURL()
 {
     let form = document.getElementById('urlForm');
     let url = form.urlInput.value;
+    // duration = form.durationInput.value;
+    // console.log(duration);
+
     form.style.display = 'none'; //hides form
     document.getElementById('stopRefresh').style.display = 'block'; //unhides stop button
 
@@ -19,7 +24,7 @@ function generateQRCode(url)
     qr.innerHTML = ``;
 
     const qrcode = new QRCode(qr, {
-        text: ("redirect.html" + '?time=' + currTime + "&url=" + url),
+        text: ("ckitzhaber.github.io/redirect.html" + '?time=' + currTime + "&url=" + url),
         width: 256,
         height: 256,
         colorDark : '#000',
@@ -28,15 +33,15 @@ function generateQRCode(url)
       });
 
     //this is for testing only
-    let link = document.createElement('a')
-    link.innerHTML = "link";
-    link.setAttribute("href", ("redirect.html" + '?time=' + currTime + "&url=" + url)); //removing my query selectors
-    console.log("redirect.html" + '?time=' + currTime + "&url=" + url);
-    document.body.appendChild(link);
+    // let link = document.createElement('a')
+    // link.innerHTML = "link";
+    // link.setAttribute("href", ("redirect.html" + '?time=' + currTime + "&url=" + url)); //removing my query selectors
+    // console.log("redirect.html" + '?time=' + currTime + "&url=" + url);
+    // document.body.appendChild(link);
+    // document.body.append(document.createElement('br'));
 
-    document.body.append(document.createElement('br'));
 
-    setTimeout(function() {generateQRCode(url)}, 5000);
+    setTimeout(function() {generateQRCode(url)}, duration);
 }
 
 function stopRefresh()
@@ -51,8 +56,25 @@ function redirectMessage()
     let url = new URL(window.location); // or construct from window.location
     
     let params = new URLSearchParams(url.search);
+    let time = parseInt(params.get('time'));
+    let redirectURL = params.get('url');
+    let currTime = Date.now();
+
     console.log(params.get('time'));
     console.log(params.get('url'));
+
+    console.log("currTime: " + currTime + " check time: " + (time + duration*2));
+
+    if(currTime < (time + duration*2))
+    {
+        console.log("redirecting");
+    }
+    else
+    {
+        let errorMessage = document.createElement('h2')
+        errorMessage.innerHTML = "Invalid Code";
+        document.body.appendChild(errorMessage);
+    }
 }
 
 /** use for redirect
